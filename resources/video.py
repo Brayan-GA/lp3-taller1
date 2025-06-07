ea5e67f77bd619d2d7ee05f012b6fff0fee8eba5
Brayan Smith <brayan@B.localdomain>
2025-06-06 20:30:30 -0500

    Implementacion del metodo patch

--- resources/video.py
+++ resources/video.py
@@ -97,6 +97,16 @@ class Video(Resource):
             VideoModel: El video actualizado
         """
         # TODO
+        video = abort_if_video_doesnt_exist(video_id)
+        args = video_update_args.parse_args()
+        if args['name']:
+            video.name = args['name']
+        if args['views']:
+            video.views = args['views']
+        if args['likes']:
+            video.likes = args['likes']
+        db.session.commit()
+        return video
         pass
     
     def delete(self, video_id):
